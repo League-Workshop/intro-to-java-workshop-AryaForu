857e7758d460de6facfa621dd8a1d229535b586d
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-27 14:52:06 -0700

    Day 3

--- src/section2/FlamingNinjaStar.java
+++ src/section2/FlamingNinjaStar.java
@@ -20,31 +20,34 @@ ineedhelp.penDown();
 		// *15. Make some adjustments to see what other kinds of shapes you can make.
 
 		// 12. Set the robot speed to 10
-ineedhelp.setSpeed(100);
+ineedhelp.setSpeed(5000);
 		// 13. Use a for loop to repeat all of the code below 25 times
-
-			// 2. Turn the robot 1/8 of a circle
-		ineedhelp.turn(365/8);
-			// 3. Move the robot 64 pixels
-ineedhelp.move(64);
-			// 4. Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			ineedhelp.turn(-40);
-			// 5. Move the robot the distance in the variable flameSize
-			ineedhelp.setPenColor(Color.ORANGE);
-			ineedhelp.move(flameSize);
-			// 6. Turn the robot 170 degrees
-			ineedhelp.turn(170);
-			// 7. Move the robot the distance in the variable flameSize (again)
-			ineedhelp.setPenColor(Color.ORANGE);
-			ineedhelp.move(flameSize);
-			// 8. Turn the robot 64 degrees to the right
-			ineedhelp.setPenColor(Color.BLACK);
-			ineedhelp.turn(64);
-			// 9. Move the robot the distance in the variable baseSize
-			ineedhelp.move(baseSize);
-			// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
+for (int i = 0; i < 25; i++) {
+	// 2. Turn the robot 1/8 of a circle
+			ineedhelp.turn(365/8);
+				// 3. Move the robot 64 pixels
+	ineedhelp.move(64);
+				// 4. Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
+				ineedhelp.turn(-40);
+				// 5. Move the robot the distance in the variable flameSize
+				ineedhelp.setPenColor(Color.ORANGE);
+				ineedhelp.move(flameSize);
+				// 6. Turn the robot 170 degrees
+				ineedhelp.turn(170);
+				// 7. Move the robot the distance in the variable flameSize (again)
+				ineedhelp.setPenColor(Color.ORANGE);
+				ineedhelp.move(flameSize);
+				// 8. Turn the robot 64 degrees to the right
+				ineedhelp.setPenColor(Color.BLACK);
+				ineedhelp.turn(64);
+				// 9. Move the robot the distance in the variable baseSize
+				ineedhelp.move(baseSize);
+				// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
+				
+				// 11. Color your ninja star like Figure 2.
 			
-			// 11. Color your ninja star like Figure 2.
+}
+			
 		
 	}
 
