9b793c050cc81a382a07d7b19b6e39f0954006bd
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-28 14:53:29 -0700

    day 4

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,21 +1,25 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score = 0;
 		// 2.  Ask the user a question 
-		
+	String question1 = JOptionPane.showInputDialog("Who is the best team in the NFL?");
 		// 3.  Use an if statement to check if their answer is correct
-		
-		// 4.  if the user's answer is correct
-		
-		// -- add one to their score 
-		
-		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
+		if(question1.equals("49ers")){
+			// 4.  if the user's answer is correct
+			JOptionPane.showMessageDialog(null, "You are correct");
+			// -- add one to their score 
+			score=score = +1;
+		}
+		else {
+	JOptionPane.showMessageDialog(null, "You obviously dont know alot about football boii ");
+			}
 		// 6.  After all the questions have been asked, print the user's score 
 		
 	}
